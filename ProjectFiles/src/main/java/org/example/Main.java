98204e8bb9bab0896729f7f882406e16e37bd2fc
deliacostea <126463814+deliacostea@users.noreply.github.com>
2024-02-27 02:10:04 +0200

    Add files via upload

---
+++ ProjectFiles/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.GUI.Interfata;
+import org.example.Logic.Operatii;
+import org.example.Model.Monom;
+import org.example.Model.Polinom;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Interfata i1=new Interfata();
+
+    }
+
+}
